88cf699921761fe312ebb72954680f0776d29496
MSI\81804 <a1929776588@gmail.com>
2022-05-27 15:17:28 +0900

    敵から勝手にプレイヤーへの攻撃 から 敵からたまが召喚されるに変更

--- Assets/Itou/SummonEnemyBarrett.cs
+++ Assets/Itou/SummonEnemyBarrett.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SummonEnemyBarrett : MonoBehaviour
+public class EnemyShoot : MonoBehaviour
 {
     public GameObject EnemyBarrett;
     //一秒ごとに弾を設置するためのもの
     [SerializeField] private float _targetTime = default;
     [SerializeField] private float _currentTime = default;
-    [SerializeField] private BaseBurret _barret = default;
-
     // Update is called once per frame
     void Update()
     {
@@ -26,7 +24,7 @@ public class SummonEnemyBarrett : MonoBehaviour
             //敵の座標を変数positionに保存
             var position = this.gameObject.transform.position;
             //弾のプレハブを作成
-            var t = Instantiate(_barret.gameObject) as GameObject;
+            var t = Instantiate(EnemyBarrett) as GameObject;
             //弾のプレハブの位置を敵の位置にする
             t.transform.position = position;
         }
