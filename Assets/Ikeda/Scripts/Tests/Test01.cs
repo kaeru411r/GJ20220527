e4a0244ff69df7565dbbe70ccd7a76a6737e1d0c
kaeru411r <ike269fr@gmail.com>
2022-05-27 13:47:22 +0900

    Enemysを配列からリストに

--- Assets/Ikeda/Scripts/Tests/Test01.cs
+++ Assets/Ikeda/Scripts/Tests/Test01.cs
@@ -12,6 +12,14 @@ public class Test01 : MonoBehaviour
         {
             Debug.Log(e);
         }
+        Debug.Log("");
+        var go = Instantiate(new GameObject());
+        var enemy = go.AddComponent<Enemy>();
+        GameManager.AddEnemy(enemy);
+        foreach (var e in es)
+        {
+            Debug.Log(e);
+        }
     }
 
     // Update is called once per frame
