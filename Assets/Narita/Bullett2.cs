8c216dbc5d5a39c15a5aede018fe173b3f0f6348
NaritaItuki <maasu0611@gmail.com>
2022-05-28 13:15:46 +0900

    Bulletの挙動

--- Assets/Narita/Bullett2.cs
+++ Assets/Narita/Bullett2.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Bullett2 : BaseBurret
 {
-
+    
     //弾のスピード
     [SerializeField] float m_speed = 3f;
-   
+
+    float _t  = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +27,16 @@ public class Bullett2 : BaseBurret
             Destroy(this.gameObject);
         }
     }
-
+     
     // Update is called once per frame
     void Update()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.left * m_speed * 1;
+          //rb.velocity = Vector2.left * m_speed ;
+       
+        //rb.velocity=Vector2.left + Vector2.up * m_speed ;
 
-
+        rb.velocity = new Vector2(-m_speed, Mathf.Sin(_t));
+        _t += Time.deltaTime * Mathf.PI;
     }
 }
