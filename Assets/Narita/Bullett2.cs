2ce66e9e4dffe7fce6e5a43ff7a79d78d79074c3
NaritaItuki <maasu0611@gmail.com>
2022-05-28 11:44:42 +0900

    GREBulletの挙動

--- Assets/Narita/Bullett2.cs
+++ Assets/Narita/Bullett2.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Bullett2 : BaseBurret
 {
+
     //弾のスピード
     [SerializeField] float m_speed = 3f;
-    /// <summary>弾の生存期間（秒）</summary>
-    [SerializeField] float m_lifeTime = 2f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -30,9 +30,8 @@ public class Bullett2 : BaseBurret
     void Update()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.left * m_speed * 10;
+        rb.velocity = Vector2.left * m_speed * 1;
 
 
     }
-
 }
